c4e56e986200a46e9027cdb6933665f24e876696
phaseharry <harrychenhc@icloud.com>
2024-11-04 21:02:50 -0500

    prints command not found for unknown commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Panicf("error reading in user input: %v", err)
+	}
+
+	// remove the trailing "\n" when we read the user input in.
+	input = strings.Trim(input, "\n")
+
+	fmt.Printf("%v: command not found\n", input)
+
+	// delimitedInput := strings.Split(input, " ")
+	// fmt.Printf("%v", delimitedInput)
 }
