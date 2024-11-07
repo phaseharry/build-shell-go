b30d4f20a2a507fd65f0f87c5e6b3e2f88338e83
phaseharry <harrychenhc@icloud.com>
2024-11-06 19:07:10 -0500

    add infinite loop so user input is always accepted

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,19 +9,21 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		log.Panicf("error reading in user input: %v", err)
+		// Wait for user input
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Panicf("error reading in user input: %v", err)
+		}
+
+		// remove the trailing "\n" when we read the user input in.
+		input = strings.Trim(input, "\n")
+
+		fmt.Printf("%v: command not found\n", input)
+
+		// delimitedInput := strings.Split(input, " ")
+		// fmt.Printf("%v", delimitedInput)
 	}
-
-	// remove the trailing "\n" when we read the user input in.
-	input = strings.Trim(input, "\n")
-
-	fmt.Printf("%v: command not found\n", input)
-
-	// delimitedInput := strings.Split(input, " ")
-	// fmt.Printf("%v", delimitedInput)
 }
