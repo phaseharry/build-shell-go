750358ebc29c91e47962a004b55ba8d76c5b5ebc
Harry Chen <harry.chen@wbd.com>
2026-08-26 12:55:24 -0400

    codecrafters submit [skip ci]

--- internal/shell/repl.go
+++ internal/shell/repl.go
@@ -6,23 +6,21 @@ import (
 )
 
 func (s *Shell) Run() int {
-	// for {
-	fmt.Fprint(s.out, "$ ")
+	for {
+		fmt.Fprint(s.out, "$ ")
 
-	line, err := s.reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprint(s.out, err.Error())
-		return 1
+		line, err := s.reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprint(s.out, err.Error())
+			return 1
+		}
+
+		// removing the \n delimiter from the actual command so it doesn't mess up std output
+		command := strings.Trim(line, "\n")
+
+		_, ok := s.builtins[command]
+		if !ok {
+			fmt.Fprintf(s.out, "%s: command not found\n", command)
+		}
 	}
-
-	// removing the \n delimiter from the actual command so it doesn't mess up std output
-	command := strings.Trim(line, "\n")
-
-	_, ok := s.builtins[command]
-	if !ok {
-		fmt.Fprintf(s.out, "%s: command not found\n", command)
-	}
-	// }
-
-	return 0
 }
